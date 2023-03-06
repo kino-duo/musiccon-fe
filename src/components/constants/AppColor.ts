4a99fc898826189a2c4c716019846d8b4018380d
Jchan <gkswocks123@gmail.com>
2023-03-06 14:50:25 +0900

    ✨ Feat : appColor 생성 - 앱에 들어갈 색상 설정은 여기서만 이루어집니다. - originColor는 로직에 관련된 이름이 아닌 색상 자체로 이름을 짓습니다 - appColor는 로직에 관련된 이름을 짓습니다. appColor를 app 전역에서 사용합니다

---
+++ src/components/constants/AppColor.ts
@@ -0,0 +1,20 @@
+// [Todo] 명세에 따라 수정 필요
+const originColor = {
+  grey1: "#F2F2F2",
+  grey2: "#434242",
+  grey3: "#333333",
+  grey4: "#222222",
+  white: "#FFFFFF",
+  black: "#000000",
+  pastelYellow: "#F3EFE0",
+  turquoise: "#22A39F",
+  blue: "#1932B9",
+  lightBlue: "#7584D5",
+};
+
+export const appColor = {
+  ...originColor,
+  backGround: originColor.grey1,
+  text: originColor.grey3,
+  primary: originColor.lightBlue,
+};
