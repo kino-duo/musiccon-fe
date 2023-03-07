b802a391e42a5c63aab0575a599abf55aef78ae7
Jchan <gkswocks123@gmail.com>
2023-03-07 12:31:32 +0900

    💡 Comment : 주석 추가

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -4,7 +4,9 @@ import { MainBanner } from "../../components/molecules";
 export const Main = () => {
   return (
     <>
+      {/* [Temp] 메인 페이지 배경도 있으면 이쁠라나? */}
       <MainBanner />
+      {/* [Todo] 캐러셀 구현하기 */}
     </>
   );
 };
