112e1e9b9084a429356bc347a930b5796c7fb807
Jchan <gkswocks123@gmail.com>
2023-03-07 13:08:34 +0900

    ✨ FEat : sizes 객체 생성 - 앱 전체에서 사용되는 컴포넌트의 width, height을 이 곳에서 관리 - 오류 방지

---
+++ src/components/constants/sizes.ts
@@ -0,0 +1,8 @@
+/**
+ * @description
+ * Specify if component sizes are used in duplicate
+ */
+export const sizes = {
+  fullHeightWithoutHeader: "100vh - 48",
+  heightHeader: 48,
+};
--- src/components/organisms/MainHeader.tsx
+++ src/components/organisms/MainHeader.tsx
@@ -5,37 +5,41 @@ import { Menu } from "../molecules";
 import { appColor } from "../constants/appColor";
 import { breakPoints } from "../constants/breakPoints";
 import { useNavigate } from "react-router-dom";
+import { Empty } from "../atoms";
 
 export const MainHeader = () => {
   const navigate = useNavigate();
   return (
-    <div
-      style={{
-        position: "fixed",
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        backgroundColor: appColor.black,
-        opacity: 0.9,
-      }}
-    >
-      <Toolbar
-        variant="dense"
-        sx={{
-          // [Temp] 재사용 많이 될 것 같음
-          display: "flex",
-          justifyContent: "space-between",
-          color: appColor.white,
+    <>
+      <div
+        style={{
+          position: "fixed",
           width: "100%",
-          maxWidth: breakPoints.lg,
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: appColor.black,
+          opacity: 0.9,
         }}
       >
-        <Button color="inherit" onClick={() => navigate("/")}>
-          Musiccon
-        </Button>
+        <Toolbar
+          variant="dense"
+          sx={{
+            // [Temp] 재사용 많이 될 것 같음
+            display: "flex",
+            justifyContent: "space-between",
+            color: appColor.white,
+            width: "100%",
+            maxWidth: breakPoints.lg,
+          }}
+        >
+          <Button color="inherit" onClick={() => navigate("/")}>
+            Musiccon
+          </Button>
 
-        <Menu />
-      </Toolbar>
-    </div>
+          <Menu />
+        </Toolbar>
+      </div>
+      <Empty height="3rem" />
+    </>
   );
 };
